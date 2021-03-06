91b84b8b7fbd5f94b75e293829b59f63ea3035dc
thinkofmia <thinkofmia@gmail.com>
2021-03-06 16:07:39 +0800

    Update julia to filter search

--- 01 Multi-Channel Contacts/01-mia.jl
+++ 01 Multi-Channel Contacts/01-mia.jl
@@ -11,18 +11,102 @@ parsedFile = JSON.parsefile("contacts.json")
 #println(parsedFile)
 
 #Create new dictionary
-results = Dict()
+userDatabase = Dict()
+#Format is as such
+# userDatabase = {
+#   userid => {
+#   "Emails" => [email1, email2],
+#   "Phones" => [phone1, phone2, phone3],
+#   "OrderIds" => [order1, order2],
+#   "totalcontacts" => totalcontacts
+#       }
+#   }
+
+function newUser(target)
+    print()
+    print("Adding new user ")
+    print(target["OrderId"])
+    userDatabase[target["OrderId"]] = Dict(
+        "Emails" => target["Email"],
+        "Phones" => target["Phone"],
+        "OrderIds" => target["OrderId"],
+        "TotalContacts" => target["Contacts"]
+    )
+
+end
+
+function addToUser(uid, target)
+    print()
+    print("Updating user ")
+    print(target["OrderId"])
+    #Emails
+    if !(target["Email"] in userDatabase[uid]["Emails"])
+        push!(userDatabase[uid]["Emails"],target["Email"])
+    elseif (isempty(userDatabase[uid]["Emails"]))#If not email record, start emails
+        userDatabase[uid]["Emails"] = [target["Email"]]
+    end
+
+    #Phones
+    if !(target["Phone"] in userDatabase[uid]["Phones"])
+        push!(userDatabase[uid]["Phones"],target["Phone"])
+    elseif (isempty(userDatabase[uid]["Phones"]))   #If no phone record, start array
+        userDatabase[uid]["Phones"] = [target["Phone"]]
+    end
+
+    #Order Id
+    if !(target["OrderId"] in userDatabase[uid]["OrderIds"])
+        push!(userDatabase[uid]["OrderIds"],target["OrderId"])
+    elseif (isempty(userDatabase[uid]["OrderIds"]))   #If no order record, start array
+        userDatabase[uid]["OrderIds"] = [target["OrderId"]]
+    end
+    
+    userDatabase[uid]["TotalContacts"]+= target["Contacts"]
+
+end
+
+#Search Criteria, user id refers to the individual user, target refers to the matching record
+function searchCriterias(uid, target)
+    #Boolean for found
+    recordFound = false
+    if (target["Email"] in userDatabase[uid]["Emails"])
+        recordFound = true
+    elseif (target["Phone"] in userDatabase[uid]["Phones"])
+        recordFound = true
+    elseif (target["OrderId"] in userDatabase[uid]["OrderIds"])
+        recordFound = true
+    end
+
+    return recordFound
+end
+
+#Find record
+function findRecord(target)
+    #variable to start search
+    searchBegin = true
+    userFound = false
+    #Get all users
+    users = keys(userDatabase)
+    #Loop for each user
+    for (user) in users
+        #Start search in target user
+        findUser = searchCriterias(user, target)
+        #if search ends, break out of loop
+        if (findUser)
+            addToUser(uid,target)
+            userFound = true
+            break
+        end
+    end
+    if (!userFound)
+        newUser(target)
+    end
+end
+
 
 #Loop for each contact in json
 for (contact) in parsedFile
     #print(contact);
-    ks = keys(results)
-    #Categorize by emails
-    if (contact["Email"] in ks)
-        push!(results[contact["Email"]],contact["Id"])
-    else
-        results[contact["Email"]] = [contact["Id"]]
-    end
+    findRecord(contact)
 end
 
-print(results)
+print(keys(userDatabase))
