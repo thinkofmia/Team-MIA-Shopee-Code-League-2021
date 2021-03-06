2760ca2864dd76e54d2332f73e6500c2548ca59d
thinkofmia <thinkofmia@gmail.com>
2021-03-06 15:10:25 +0800

    Sort by email

--- 01 Multi-Channel Contacts/01-mia.jl
+++ 01 Multi-Channel Contacts/01-mia.jl
@@ -2,10 +2,27 @@
 #import Pkg; Pkg.add("DataFrames")
 #import Pkg; Pkg.add("JSONTables")
 
-#Using the following
+#Using the following modules
 using JSON
 using DataFrames, JSONTables
 
+#Parse file
 parsedFile = JSON.parsefile("contacts.json")
+#println(parsedFile)
 
-println(parsedFile)
+#Create new dictionary
+results = Dict()
+
+#Loop for each contact in json
+for (contact) in parsedFile
+    #print(contact);
+    ks = keys(results)
+    #Categorize by emails
+    if (contact["Email"] in ks)
+        push!(results[contact["Email"]],contact["Id"])
+    else
+        results[contact["Email"]] = [contact["Id"]]
+    end
+end
+
+print(results)
