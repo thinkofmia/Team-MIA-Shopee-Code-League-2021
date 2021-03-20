8fb4f884cec25e1a5bb08c35eea4676d45626b36
thinkofmia <thinkofmia@gmail.com>
2021-03-20 14:22:34 +0800

    Added base file for divider

---
+++ 03 Programming Contest/divider.jl
@@ -0,0 +1,14 @@
+#=
+Problem: Divider
+Max. score: 20
+
+Shopee has N software engineers. 
+Shopee accommodates them by arranging N tables on a 1D plane. 
+However, many people raise concerns that the work environment is too noisy. 
+To mitigate this issue, Shopee decides to group the engineers into K groups.
+A group is a non-overlapping segment of contiguous engineers. 
+Shopee will then put dividers between the groups.
+
+Shopee wants to minimize the total noise value. 
+Please help Shopee to find the minimum total noise value possible.
+=#
