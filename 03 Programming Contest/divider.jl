317dbd7044b284ce0a1da2112baffb9039712cf1
thinkofmia <thinkofmia@gmail.com>
2021-03-20 14:56:21 +0800

    Create file to read inputs from jl

--- 03 Programming Contest/divider.jl
+++ 03 Programming Contest/divider.jl
@@ -12,3 +12,33 @@ Shopee will then put dividers between the groups.
 Shopee wants to minimize the total noise value. 
 Please help Shopee to find the minimum total noise value possible.
 =#
+
+
+# Julia program to calculate sum of  
+# 5 integers obtained from console/user input 
+
+#Variables
+line1 = ""
+line2 = ""
+  
+# Prompt to enter 1st line
+println("Enter N and K respectively on same line: ") 
+
+line1 = readline()
+inputArr = split(line1, " ")
+N = parse(Int64, inputArr[1]) 
+K = parse(Int64, inputArr[2]) 
+println("N:", N)
+println("K:", K)
+
+# Prompt to enter 2nd line
+println("Enter ",N," numbers in a line") 
+  
+line2 = readline()
+inputArr = split(line2, " ")
+allEng = []
+# Taking Input from user 
+for i in 1:N
+   push!(allEng,inputArr[i])
+end 
+println("Engineers: ",line2)
